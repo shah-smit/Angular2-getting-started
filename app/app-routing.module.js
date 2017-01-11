f4ba7020ce5799cd7be0f6a8be4b07d64b373c9b
Smit <smitshah95@hotmail.com>
2017-01-11 19:27:39 +0800

    added routing

--- app/app-routing.module.js
+++ app/app-routing.module.js
@@ -9,31 +9,20 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var platform_browser_1 = require('@angular/platform-browser');
-var forms_1 = require('@angular/forms');
-var app_component_1 = require('./app.component');
-var http_1 = require('@angular/http');
-var app_routing_module_1 = require('./app-routing.module');
-var AppModule = (function () {
-    function AppModule() {
+var router_1 = require('@angular/router');
+var routes = [];
+var AppRoutingModule = (function () {
+    function AppRoutingModule() {
     }
-    AppModule = __decorate([
+    AppRoutingModule = __decorate([
         core_1.NgModule({
-            imports: [
-                platform_browser_1.BrowserModule,
-                forms_1.FormsModule,
-                app_routing_module_1.AppRoutingModule,
-                http_1.HttpModule
-            ],
-            declarations: [
-                app_component_1.AppComponent,
-                app_routing_module_1.routableComponents
-            ],
-            bootstrap: [app_component_1.AppComponent]
+            imports: [router_1.RouterModule.forRoot(routes, { useHash: true })],
+            exports: [router_1.RouterModule]
         }), 
         __metadata('design:paramtypes', [])
-    ], AppModule);
-    return AppModule;
+    ], AppRoutingModule);
+    return AppRoutingModule;
 }());
-exports.AppModule = AppModule;
-//# sourceMappingURL=app.module.js.map
+exports.AppRoutingModule = AppRoutingModule;
+exports.routableComponents = [];
+//# sourceMappingURL=app-routing.module.js.map
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,25 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }  from '@angular/forms';
 import { AppComponent }   from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http'; 
+
+import {AppRoutingModule, routableComponents } from './app-routing.module';
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpModule
+  ],
+  declarations: [
+    AppComponent,
+    routableComponents
+  ], 
+  bootstrap: [ AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
